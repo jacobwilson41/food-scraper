0fe04fb0dbb956bb6b0fefe35e5b3867eca7470e
Jacob Wilson <jacobcharles96@gmail.com>
2020-04-07 15:38:03 -0400

    Browser window opens and navigates to a url

---
+++ app.js
@@ -0,0 +1,8 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({headless: false, slowmo: true, defaultViewport: null});
+  const page = await browser.newPage();
+  await page.goto('https://www.cleaneatz.com/');
+
+})();
