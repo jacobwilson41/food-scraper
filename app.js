86da4aa4a8083b10f1765c404b2e6507e7899cd7
Jacob Wilson <jacobcharles96@gmail.com>
2020-04-17 14:55:14 -0400

    Changed url to mealplanmenu path

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch({headless: false, slowmo: true, defaultViewport: null});
   const page = await browser.newPage();
-  await page.goto('https://www.cleaneatz.com/');
+  await page.goto('https://www.cleaneatz.com/mealplanmenu');
 
+  browser.close();
 })();
