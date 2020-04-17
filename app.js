9c7a44abbd35d2f0f4f2d21d3048bd3b03733d77
Jacob Wilson <jacobcharles96@gmail.com>
2020-04-17 17:36:40 -0400

    App selects state and cafe

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 const puppeteer = require('puppeteer');
 
-(async (state, cafe) => {
+(async () => {
 
-  state = state.toUpperCase();
-  cafe = cafe.toUpperCase();
   const url = 'https://www.cleaneatz.com/mealplanmenu';
 
-  const browser = await puppeteer.launch({headless: false, slowmo: true, defaultViewport: null});
+  const browser = await puppeteer.launch({headless: true, slowmo: false, defaultViewport: null});
 
   const context = browser.defaultBrowserContext();
   await context.overridePermissions(url, ['geolocation']);
 
   const page = await browser.newPage();
   await page.goto(url);
-  await page.waitForSelector('select');
+  await page.waitForSelector('select')
+  await page.waitFor(100);
 
-  const states = await page.$('#location-states');
-  states.click();
+  const state = await page.select('select#location-states', 'SC');
+  const cafe = await page.select('select.fancy-select.state-locs', '3576171');
+
 
 
 
 
 
   // browser.close();
-})('South Carolina', 'Greenville');
+})();
