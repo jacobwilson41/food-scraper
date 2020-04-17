53b5c81891715e1295e01833f3759a57b788757c
Jacob Wilson <jacobcharles96@gmail.com>
2020-04-17 16:02:00 -0400

    App clicks on State dropdown

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const puppeteer = require('puppeteer');
 
-(async () => {
-  const browser = await puppeteer.launch({headless: false, slowmo: true, defaultViewport: null});
-  const page = await browser.newPage();
-  await page.goto('https://www.cleaneatz.com/mealplanmenu');
+(async (state, cafe) => {
 
-  browser.close();
-})();
+  state = state.toUpperCase();
+  cafe = cafe.toUpperCase();
+  const url = 'https://www.cleaneatz.com/mealplanmenu';
+
+  const browser = await puppeteer.launch({headless: false, slowmo: true, defaultViewport: null});
+
+  const context = browser.defaultBrowserContext();
+  await context.overridePermissions(url, ['geolocation']);
+
+  const page = await browser.newPage();
+  await page.goto(url);
+  await page.waitForSelector('select');
+
+  const states = await page.$('#location-states');
+  states.click();
+
+
+
+
+
+  // browser.close();
+})('South Carolina', 'Greenville');
